346cafcbac1aa456d52ada00bf6c1dcf0771fd88
Newave-agc <76753419+Newave-agc@users.noreply.github.com>
2023-01-18 10:00:32 +0200

    last one I hope hihihi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 8080;
 const socket = require("socket.io");
 const cors = require("cors");
 
