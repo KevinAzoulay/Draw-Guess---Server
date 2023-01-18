0e0b8a3bd2f339afae975ab3a74a1a2e8b1fde52
Newave-agc <76753419+Newave-agc@users.noreply.github.com>
2023-01-18 03:11:37 +0200

    env file added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 5000 || 8080;
 const socket = require("socket.io");
 const cors = require("cors");
 
